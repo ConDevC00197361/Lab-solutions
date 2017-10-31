2dd7e01376801f86c036f05fa6012b89e414f55b
ConDevC00197361 <zoltan.fuzesi.eu@gmail.com>
2017-10-31 19:45:03 +0000

    lab5 files added

---
+++ Lab5/SafeBuffer.cpp
@@ -0,0 +1,51 @@
+// SafeBuffer.cpp --- 
+// 
+// Filename: SafeBuffer.cpp
+// Description: 
+// Author: Zoltan Fuzesi 
+// Maintainer: 
+// Created: Tue Oct 31 19:43:59 2017 (+0000)
+// Version: 
+// Package-Requires: ()
+// Last-Updated: 
+//           By: 
+//     Update #: 0
+// URL: 
+// Doc URL: 
+// Keywords: 
+// Compatibility: 
+// 
+// 
+
+// Commentary: 
+// 
+// 
+// 
+// 
+
+// Change Log:
+// 
+// 
+// 
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+
+
+
+// 
+// SafeBuffer.cpp ends here
